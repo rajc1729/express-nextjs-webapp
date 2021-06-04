f18d9ab6eea50b05b2cedcee0a5db5e59ffad279
rajc1729gitcommitexitgit config --global user.name rajc1729 <chhatbarraj@csu.fullerton.edu>
2021-06-04 01:31:00 -0700

    Serving Cache Results

--- backend/models/country.model.js
+++ backend/models/country.model.js
@@ -11,37 +11,16 @@ module.exports = (mongoose) => {
       region: String,
       flag: String,
       population: Number,
+      createdAt: { type: Date, expires: 10, default: Date.now },
     },
     { timestamps: true }
   );
-  countrySchema.index({ code: 1, region: 1 });
+  countrySchema.index(
+    { code: 1, region: 1, createdAt: 1 },
+    { expireAfterSeconds: 10 }
+  );
 
   const countryModel = mongoose.model("country", countrySchema);
 
   return countryModel;
 };
-
-// const Schema = require("mongoose").Schema;
-// const countrySchema = new mongoose.Schema({
-//   name: String,
-//   code: {
-//     type: String,
-//     required: true,
-//     unique: true,
-//   },
-//   capital: String,
-//   region: {
-//     type: String,
-//     required: true,
-//     unique: true,
-//   },
-//   flag: String,
-//   population: Number,
-//   created: {
-//     type: Date,
-//     default: Date.now,
-//   },
-// });
-
-// const countryModel = db.model("Country", countrySchema);
-// module.exports = countryModel;
--- backend/utility/middlewares.js
+++ backend/utility/middlewares.js
@@ -3,44 +3,53 @@ const db = require("../models");
 const countryModel = db.countryModel;
 
 exports.checkCacheStatus = (req, res, next) => {
-  axios
-    .get("https://restcountries.eu/rest/v2/all")
-    .then((res) => {
-      if (res.status == 200) {
-        const countries = res.data;
-        const promises = [];
-        // console.log(countries);
-        countries.forEach((country) => {
-          const countryUpdateData = {
-            name: country.name,
-            code: country.alpha3Code,
-            capital: country.capital,
-            region: country.region,
-            flag: country.flag,
-            population: country.population,
-          };
-
-          promises.push(
-            countryModel.findOneAndUpdate(
-              { code: country.alpha3Code },
-              countryUpdateData,
-              {
-                useFindAndModify: false,
-                upsert: true,
-              }
-            )
-          );
-        });
-
-        return Promise.all(promises);
-      }
-    })
-    .then(() => {
+  countryModel.find({}, function (err, countries) {
+    console.log(countries.length);
+    if (err) {
+      console.log("Error getting the countries");
+    } else if (countries.length !== 0) {
       next();
-    })
-    .catch((err) => {
-      res.status(500).send({
-        message: err.message || "Error retrieving countries from api",
-      });
-    });
+    } else {
+      axios
+        .get("https://restcountries.eu/rest/v2/all")
+        .then((res) => {
+          if (res.status == 200) {
+            const countries = res.data;
+            console.log("Request sent");
+            const promises = [];
+            countries.forEach((country) => {
+              const countryUpdateData = {
+                name: country.name,
+                code: country.alpha3Code,
+                capital: country.capital,
+                region: country.region,
+                flag: country.flag,
+                population: country.population,
+              };
+
+              promises.push(
+                countryModel.findOneAndUpdate(
+                  { code: country.alpha3Code },
+                  countryUpdateData,
+                  {
+                    useFindAndModify: false,
+                    upsert: true,
+                  }
+                )
+              );
+            });
+
+            return Promise.all(promises);
+          }
+        })
+        .then(() => {
+          next();
+        })
+        .catch((err) => {
+          res.status(500).send({
+            message: err.message || "Error retrieving countries from api",
+          });
+        });
+    }
+  });
 };
